87667ff1ada09a3268de95ba6fe975e52f96b531
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-14 09:50:47 -0700

    Add pacman to project list

--- portfolio.js
+++ portfolio.js
@@ -4,17 +4,24 @@ const reactImage = 'images/react-logo.svg';
 const portfolio = [
   {
     title: 'Designer',
-    site: 'http://designer.7i5nptapg2.us-west-2.elasticbeanstalk.com/',
+    site: null,
     github: 'https://github.com/bananno/designer',
     tags: 'react',
     description: 'Use tools to design a website layout',
   },
   {
     title: 'Ancestry',
-    site: 'http://ancestry.cp3msbrmnj.us-west-2.elasticbeanstalk.com/',
+    site: null,
     github: 'https://github.com/bananno/acpancestry',
     tags: '',
     description: 'My genealogy research',
+  },
+  {
+    title: 'Pac-Man',
+    site: 'http://annacpeterson.com/pacman/',
+    github: 'https://github.com/bananno/pacman',
+    tags: '',
+    description: 'Pac-Man game in JavaScript',
   },
   {
     title: 'Sliding Puzzle',
@@ -25,7 +32,7 @@ const portfolio = [
   },
   {
     title: 'Dragon Drop Puzzle',
-    site: 'http://dragon-drop.xyetrvyk25.us-west-2.elasticbeanstalk.com/',
+    site: null,
     github: 'https://github.com/bananno/dragon-drop',
     tags: 'collaborative',
     description: 'A tile-matching puzzle game',
