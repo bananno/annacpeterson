d4b8faff8eca94c2a752852da44475214f99ed8e
Anna Peterson <peterson.anna.c@gmail.com>
2019-07-19 19:01:11 -0700

    Move portfolio to subdirectory; update project list; create favicon

---
+++ portfolio/portfolio.js
@@ -0,0 +1,43 @@
+
+const reactImage = 'images/react-logo.svg';
+
+const portfolio = [
+  {
+    title: 'Pac-Man',
+    site: 'https://annacpeterson.com/pacman/',
+    github: 'https://github.com/bananno/pacman',
+    description: 'Pac-Man game in JavaScript',
+  },
+  {
+    title: 'Ancestry',
+    site: 'https://ancestry.annacpeterson.com',
+    github: 'https://github.com/bananno/acpancestry',
+    description: 'My genealogy research',
+  },
+  {
+    title: 'Sliding Puzzle',
+    site: 'https://annacpeterson.com/slidingpuzzle',
+    github: 'https://github.com/bananno/slidingpuzzle',
+    description: 'Classic tile ordering game',
+  },
+  {
+    title: 'Civilization',
+    site: null,
+    github: 'https://github.com/bananno/civilization',
+    description: 'Turn-based game in Express & MongoDB',
+  },
+  {
+    title: 'Hatch Dragons',
+    site: null,
+    tags: 'react',
+    github: 'https://github.com/bananno/hatch-dragons',
+    description: 'Virtual pet dragon game in Express & React',
+  },
+  {
+    title: 'Designer',
+    site: null,
+    tags: 'react',
+    github: 'https://github.com/bananno/designer',
+    description: 'Use tools to design a website layout',
+  },
+];
